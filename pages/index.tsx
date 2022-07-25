466e75c68351a2bea7abf3734c271eb41e7279c9
Tzu Yin <tzyn.wang@gmail.com>
2022-07-25 21:33:52 +0800

    [feat] update Home tags, layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,15 +15,24 @@ function Home(props: HomeProps): React.ReactElement {
   return (
     <React.Fragment>
       {allPostsData.map(({ id, date, title, category, htmlContent }) => (
-        <React.Fragment key={id}>
-          <div className="text-xl">title: {title}</div>
-          <div className="text-xl">date: {date}</div>
-          <div className="text-xl">category: {category}</div>
+        <section key={id}>
+          <h1 className="text-3xl">{title}</h1>
+          <div className="text-xl my-1">{date}</div>
+          <div className="inline-flex space-x-2">
+            {category.map((c, index) => (
+              <span
+                key={index}
+                className="px-2 py-1 rounded-md bg-secondary-content text-sm"
+              >
+                {c}
+              </span>
+            ))}
+          </div>
           <div
             className="postContentWrapper"
             dangerouslySetInnerHTML={{ __html: htmlContent }}
           />
-        </React.Fragment>
+        </section>
       ))}
     </React.Fragment>
   );
