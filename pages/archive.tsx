f3e2ef01f29389ab3ffa78e576cb74b56461e864
Tzu Yin <tzyn.wang@gmail.com>
2023-02-03 16:46:34 +0800

    [feat] no need to add <a> inside <Link />

--- pages/archive.tsx
+++ pages/archive.tsx
@@ -17,21 +17,19 @@ export default function ArchivePage(props: ArchivePageProps) {
       <ul>
         {allPostsYearAndTitle.map(({ id, date, category }) => (
           <li key={id}>
-            <Link href={`/${date}/${id}`}>
-              <a className="inline-flex space-x-2">
-                <span>{date}</span>
-                <span className="underline">{id}</span>
-                <div className="inline-flex space-x-2">
-                  {category.map((c, i) => (
-                    <span
-                      key={i}
-                      className="px-2 py-1 rounded-md bg-secondary-content text-sm"
-                    >
-                      {c}
-                    </span>
-                  ))}
-                </div>
-              </a>
+            <Link href={`/${date}/${id}`} className="inline-flex space-x-2">
+              <span>{date}</span>
+              <span className="underline">{id}</span>
+              <div className="inline-flex space-x-2">
+                {category.map((c, i) => (
+                  <span
+                    key={i}
+                    className="px-2 py-1 rounded-md bg-secondary-content text-sm"
+                  >
+                    {c}
+                  </span>
+                ))}
+              </div>
             </Link>
           </li>
         ))}
