3ed8374e8c0e93cbac4547b27c2966617b8a7d57
Tzu Yin <tzyn.wang@gmail.com>
2023-02-04 17:18:55 +0800

    [fix] timeFormat args type

--- lib/time-format.ts
+++ lib/time-format.ts
@@ -1,6 +1,6 @@
 import dayjs from 'dayjs';
 import { DATE_TIME_FORMAT } from '@Model/GeneralModels';
 
-export default function timeFormat(rawDate: string) {
+export default function timeFormat(rawDate?: dayjs.ConfigType) {
   return dayjs(rawDate).format(DATE_TIME_FORMAT);
 }
