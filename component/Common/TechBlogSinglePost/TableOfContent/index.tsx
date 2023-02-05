310bfad224f33db3174da8ee90ee042a9203a005
Tzu Yin <tzyn.wang@gmail.com>
2023-02-05 13:08:30 +0800

    [feat] tech post toc basic styling

--- component/Common/TechBlogSinglePost/TableOfContent/index.tsx
+++ component/Common/TechBlogSinglePost/TableOfContent/index.tsx
@@ -3,6 +3,7 @@ import cn from 'classnames';
 import Link from 'next/link';
 import Typography from '@mui/material/Typography';
 import useI18n from '@Hook/useI18n';
+import { MAX_HEADING_LEVEL_IN_TECH_POST } from '@Model/GeneralModels';
 import scopedStyles from './index.module.css';
 import type { TableOfContentProps } from './types';
 
@@ -11,6 +12,13 @@ function TableOfContent(props: TableOfContentProps): React.ReactElement {
   const { tableOfContents } = props;
   const i18n = useI18n();
 
+  /* Functions */
+  const getHeadingIndent = (headingLevel: number) => {
+    return headingLevel - MAX_HEADING_LEVEL_IN_TECH_POST > 0
+      ? `${(headingLevel - MAX_HEADING_LEVEL_IN_TECH_POST) * 16}px`
+      : 0;
+  };
+
   /* Main */
   return (
     <div>
@@ -18,9 +26,14 @@ function TableOfContent(props: TableOfContentProps): React.ReactElement {
         {i18n.t('frontend.techBlogSinglePost.toc')}
       </Typography>
       <ul className={cn(scopedStyles.toc_container)}>
-        {tableOfContents.map((content, index) => (
-          <li key={index}>
-            <Link href={`#${content.to}`}>{content.postSubTitle}</Link>
+        {tableOfContents.map(({ depth, to, postSubTitle }, index) => (
+          <li
+            key={index}
+            style={{
+              paddingLeft: getHeadingIndent(depth),
+            }}
+          >
+            <Link href={to}>{postSubTitle}</Link>
           </li>
         ))}
       </ul>
