7d7cddb4bf35721f2aba968a2ddd556264d5863f
Tzu Yin <tzyn.wang@gmail.com>
2023-02-11 09:16:03 +0800

    [feat] _app allow mdx content

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import React, { memo } from 'react';
+import { MDXProvider } from '@mdx-js/react';
+import H1Title from '@Component/Common/H1Title';
 import MainAppView from '@Component/Layer/MainAppView';
 import MuiThemeProvider from '@Component/Layer/MuiThemeProvider';
 import '@Style/globals.css';
 import type { AppProps } from 'next/app';
+
+const components = {
+  h2: H1Title,
+};
 
 function AppEntry(AppEntryProps: AppProps): React.ReactElement {
   /* States */
@@ -12,7 +18,9 @@ function AppEntry(AppEntryProps: AppProps): React.ReactElement {
   return (
     <MuiThemeProvider>
       <MainAppView>
-        <Component {...pageProps} />
+        <MDXProvider components={components}>
+          <Component {...pageProps} />
+        </MDXProvider>
       </MainAppView>
     </MuiThemeProvider>
   );
