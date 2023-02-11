e963562a9418045573c8032d121bd32259e121e9
Tzu Yin <tzyn.wang@gmail.com>
2023-02-11 22:05:57 +0800

    [fix] useGetTagPair sort by alphabetical order

--- hook/useGetTagPair.ts
+++ hook/useGetTagPair.ts
@@ -7,7 +7,7 @@ export default function useGetTagPair(
   posts: TechPostIdDateYearTags
 ): TagCountPairs {
   const allTags = posts.map(({ tag }) => tag).flat(2);
-  const allTagsUnique = Array.from(new Set(allTags));
+  const allTagsUnique = Array.from(new Set(allTags)).sort();
   return allTagsUnique.map((tagName) => ({
     tagName,
     count: allTags.filter((tag) => tag === tagName).length,
