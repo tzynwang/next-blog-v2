9d83529594061eaafcbf5bc99d78b43eb217f3e4
Tzu Yin <tzyn.wang@gmail.com>
2023-02-11 11:29:17 +0800

    [feat] next.config remove mdx

--- next.config.js
+++ next.config.js
@@ -1,15 +1,13 @@
 const alias = require('./config/alias');
-const mdxRules = require('./config/mdxRules');
 const svgRules = require('./config/svgRules');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
+  pageExtensions: ['md', 'js', 'tsx', 'ts'],
   reactStrictMode: true,
   swcMinify: true,
   webpack: (config, options) => {
     config.module.rules.push({
-      ...mdxRules(options),
       ...svgRules,
     });
     config.resolve.alias = {
