fea2bb39f2d15d58dd59e78f2e380f7fca008fa4
Tzu Yin <tzyn.wang@gmail.com>
2023-02-11 14:22:54 +0800

    [feat] use css module for RWD styling

--- component/Common/TechBlogSinglePost/BackToTop/index.tsx
+++ component/Common/TechBlogSinglePost/BackToTop/index.tsx
@@ -3,13 +3,14 @@ import cn from 'classnames';
 import Fab from '@mui/material/Fab';
 import Zoom from '@mui/material/Zoom';
 import ExpandLessIcon from '@Asset/expand_less_black_24dp.svg';
-import useMuiThemeStyle from '@Hook/useMuiThemeStyle';
 import useShouldShowFab from '@Hook/useShouldShowFab';
+import theme, { useMediaQuery } from '@Style/mui/index';
+import scopedStyles from './index.module.css';
 
 function BackToTop(): React.ReactElement {
   /* States */
-  const muiThemeStyles = useMuiThemeStyle();
   const shouldShowFab = useShouldShowFab();
+  const breakpointsUpSm = useMediaQuery(theme.breakpoints.up('sm'));
 
   /* Functions */
   const backToTop = (): void => {
@@ -21,7 +22,10 @@ function BackToTop(): React.ReactElement {
     <Zoom in={shouldShowFab}>
       <Fab
         aria-label="back-to-page-top"
-        className={cn(muiThemeStyles.fabPosition)}
+        className={cn(
+          scopedStyles.fab_position,
+          breakpointsUpSm && scopedStyles.fab_position_up_sm
+        )}
         color="primary"
         size="medium"
         onClick={backToTop}
